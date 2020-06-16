128f29ab8812ce90b299edbb8ecca38276b2655e
Philip Kouchner <philipk179@gmail.com>
2020-06-16 16:03:05 -0700

    all routes working on server side

--- routes/api_route.js
+++ routes/api_route.js
@@ -6,17 +6,12 @@ const read = util.promisify(fs.readFile);
 const write = util.promisify(fs.writeFile);
 
 router.get("/api/notes", async (req, res) => {
-
-    console.log("I'm here get request");
     let noteTask = await read("./db/db.json");
     const { note } = JSON.parse(noteTask);
-    console.log(note);
     res.json(note);
-
 });
 
 router.post("/api/notes", async (req, res) => {
-
     let data = await read("./db/db.json");
     data = JSON.parse(data);
     let idNote = data.note.length + 1;
@@ -27,30 +22,15 @@ router.post("/api/notes", async (req, res) => {
     res.json(data);
 });
 
-
 router.delete("/api/notes/:id", async (req, res) => {
-
-    console.log("I'm here delete request");
     let data = await read("./db/db.json");
     data = JSON.parse(data);
-
-    let { note } = data;
-    console.log(req.params);
+    let { note } = data;;
     const noteToDelete = parseInt(req.params.id);
-    console.log(typeof noteToDelete);
-
-    //const newNote = note.filter(deleteNote => {
-
-    //     return noteToDelete !== deleteNote.id;
-    // });
     const newNote = note.findIndex(deleteNote => noteToDelete === deleteNote.id);
-
     note.splice(newNote, 1);
-    // const newObj = {};
-    // newObj.note = newNote;
     await write("./db/db.json", JSON.stringify(data, null, 2));
     res.json(data);
-
 });
 
 module.exports = router;
