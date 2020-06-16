ddd9f9b7d6b0bdec8b86cd9f06b49430fd431b7a
Philip Kouchner <philipk179@gmail.com>
2020-06-15 21:12:12 -0700

    add post delete route

--- routes/api_route.js
+++ routes/api_route.js
@@ -3,13 +3,32 @@ const router = express.Router();
 const fs = require("fs");
 const util = require("util");
 const read = util.promisify(fs.readFile);
+const write = util.promisify(fs.writeFile);
 
 router.get("/api/notes", async (req, res) => {
 
-    console.log("I'm here");
+    console.log("I'm here get request");
     let note = await read("./db/db.json");
     note = JSON.parse(note);
     res.json(note);
+
+});
+
+router.post("/api/notes", async (req, res) => {
+
+    console.log("I'm here post request");
+    let note = await JSON.parse(read("./db/db.json"));
+    const id = note.length + 1;
+    console.log(req.query);
+    // note = JSON.parse(note);
+    // res.json(note);
+
+});
+
+router.delete("/api/notes", async (req, res) => {
+
+    console.log("I'm here delete request");
+
 
 });
 
